a70f8ac98dae19a66d51a29af9f1895951ba61b7
Irwan Setiawan <irwan@irwansetiawan.com>
2023-07-13 23:32:39 +0800

    HTML formatting

--- src/static/index.ts
+++ src/static/index.ts
@@ -1,5 +1,9 @@
 const sourceImg = document.getElementById('sourceImg') as HTMLImageElement;
 const sourceInput = document.getElementById('source') as HTMLInputElement;
+sourceImg.addEventListener('load', (event) => {
+    const thisImg = event.currentTarget as HTMLImageElement;
+    console.log('Image size: ', thisImg.clientWidth, thisImg.clientHeight)
+});
 sourceInput?.addEventListener('change', (event) => {
     const sourceFile = sourceInput?.files?.[0];
     if (sourceFile) {
@@ -9,6 +13,10 @@ sourceInput?.addEventListener('change', (event) => {
 
 const targetImg = document.getElementById('targetImg') as HTMLImageElement;
 const targetInput = document.getElementById('target') as HTMLInputElement;
+targetImg.addEventListener('load', (event) => {
+    const thisImg = event.currentTarget as HTMLImageElement;
+    console.log('Image size: ', thisImg.clientWidth, thisImg.clientHeight)
+});
 targetInput?.addEventListener('change', (event) => {
     const targetFile = targetInput?.files?.[0];
     if (targetFile) {
@@ -21,10 +29,18 @@ form.addEventListener('submit', (event) => {
     const form = event.currentTarget as HTMLFormElement;
     const url = new URL(form.action);
     const formData = new FormData(form);
-    const fetchOptions = {
+    const fetchOptions: RequestInit = {
         method: form.method,
         body: formData,
     };
-    fetch(url, fetchOptions);
+    const responseDiv = document.getElementById('response') as HTMLDivElement;
+    fetch(url, fetchOptions)
+        .then(async (res) => {
+            const responseJson = JSON.parse(await res.text());
+            responseDiv.innerHTML = JSON.stringify(responseJson, null, 4);
+        })
+        .catch((error) => {
+            responseDiv.innerHTML = error;
+        });
     event.preventDefault();
 });
